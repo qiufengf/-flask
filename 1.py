9f17de84618434ea81576122e8885e3c4004629d
ldh <1901151259@qq.com>
2025-01-13 20:33:09 +0800

    Add minimal home page

---
+++ 1.py
@@ -0,0 +1,4 @@
+five_men_fight_bg = {'ian','alex','佩奇','Old_shang','之隔'}
+happy_day = {'李晓琪','ian'}
+
+print(five_men_fight_bg.intersection(happy_day))
---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import url_for, Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
