626312bedcfbcdf6e89e4841b58efc6ef38d708f
ldh <1901151259@qq.com>
2025-01-13 20:48:19 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-from flask import url_for, Flask
+from flask import url_for, Flask,render_template
 from markupsafe import escape
 
+
+# 准备虚拟数据
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello'
-
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User: {escape(name)}'
-
-@app.route('/test')
-def test_url_for():
-    # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
-    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
-    print(url_for('user_page', name='peter'))  # 输出：/user/peter
-    print(url_for('test_url_for'))  # 输出：/test
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-    return 'Test page'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
 if __name__ == '__main__':
     app.run(debug=True)
